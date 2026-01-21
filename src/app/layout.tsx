e2c5d88fcbd8807bc6f294ea6639a508080d1a37
punnawat <punnawat@unxidev.co.th>
2026-01-21 09:59:54 +0700

    Add @vercel/analytics and @vercel/speed-insights for enhanced performance tracking and analytics. Update package.json and pnpm-lock.yaml to include new dependencies.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { UIStateProvider } from "../context/ui-state-context";
 import "./globals.css";
 
@@ -88,6 +89,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} bg-[#08110c] text-white antialiased`}
       >
         <UIStateProvider>{children}</UIStateProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
